2c87e491019f15112c0be087a7f8fe4fe2d9ce26
Ryan Andri <ryanandri@linuxmail.org>
2018-07-16 23:42:03 +0700

    santoni: add touchscreen configs from MIUI 10 8.7.12
    Change-Id: Ibb2edea9a9c618887899f541133c8907124127fd

--- santoni/santoni-vendor.mk
+++ santoni/santoni-vendor.mk
@@ -441,6 +441,8 @@ PRODUCT_PACKAGES += \
     libts_face_beautify_hal
 PRODUCT_COPY_FILES += \
     vendor/xiaomi/santoni/proprietary/bin/dpmd:system/bin/dpmd \
+    vendor/xiaomi/santoni/proprietary/etc/ft5336_biel_1080p.ini:system/etc/ft5336_biel_1080p.ini \
+    vendor/xiaomi/santoni/proprietary/etc/ft5336_oufei_720p.ini:system/etc/ft5336_oufei_720p.ini \
     vendor/xiaomi/santoni/proprietary/etc/cne/SwimConfig.xml:system/etc/cne/SwimConfig.xml \
     vendor/xiaomi/santoni/proprietary/etc/cne/andsfCne.xml:system/etc/cne/andsfCne.xml \
     vendor/xiaomi/santoni/proprietary/etc/dpm/dpm.conf:system/etc/dpm/dpm.conf \
